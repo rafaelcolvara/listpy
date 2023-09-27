66190f257ff4f353775f8bd9cec230516bfcd5ea
Rafael Colvara <rafael.colvara@valemobi.com.br>
2023-09-27 18:38:26 -0300

    all

---
+++ todoapp/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from .models import Todo
+from .forms import TodoForm
+
+def todo_list(request):
+    todos = Todo.objects.all()
+    return render(request, 'todoapp/todo_list.html', {'todos': todos})
+
+def todo_create(request):
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('todo_list')
+    else:
+        form = TodoForm()
+    return render(request, 'todoapp/todo_form.html', {'form': form})
+
+def todo_update(request, pk):
+    todo = Todo.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid():
+            form.save()
+            return redirect('todo_list')
+    else:
+        form = TodoForm(instance=todo)
+    return render(request, 'todoapp/todo_form.html', {'form': form})
+
+def todo_delete(request, pk):
+    todo = Todo.objects.get(pk=pk)
+    if request.method == 'POST':
+        todo.delete()
+        return redirect('todo_list')
+    return render(request, 'todoapp/todo_confirm_delete.html', {'todo': todo})
